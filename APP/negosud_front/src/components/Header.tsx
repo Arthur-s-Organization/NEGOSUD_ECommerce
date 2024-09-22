2a25c03bbdec7f549d26b2177386fc59649731e7
JeanneGrenet <jeanne.grenet@gmail.com>
2024-09-22 22:52:02 +0200

    fix : style and responsive

--- APP/negosud_front/src/components/Header.tsx
+++ APP/negosud_front/src/components/Header.tsx
@@ -1,53 +1,87 @@
-import { SearchIcon, ShoppingCartIcon, UserIcon } from "lucide-react";
+import { MenuIcon, SearchIcon, ShoppingCartIcon, UserIcon } from "lucide-react";
 import { Input } from "@/components/ui/input";
-import { Button } from "@/components/ui/button";
 import Image from "next/image";
 import horizontalLogo from "/public/LogoHorizontal.svg";
+import Link from "next/link";
+import { Dialog, DialogContent, DialogTrigger } from "./ui/dialog";
+import { Button } from "./ui/button";
 
 export default function Header() {
+  const NavItems = () => (
+    <>
+      <li>
+        <Link href="#" className="hover:text-secondary transition-colors">
+          Produits
+        </Link>
+      </li>
+      <li>
+        <Link href="#" className="hover:text-secondary transition-colors">
+          A propos
+        </Link>
+      </li>
+      <li>
+        <Link href="#" className="hover:text-secondary transition-colors">
+          Contact
+        </Link>
+      </li>
+    </>
+  );
+
+  const SearchBar = () => (
+    <>
+      <Input
+        type="search"
+        placeholder="Rechercher des vins ..."
+        className="pl-10 pr-4 py-2 w-64 rounded-full bg-white text-black"
+      />
+      <SearchIcon
+        className="absolute left-3 top-1/2 transform -translate-y-1/2 text-gray-400"
+        size={20}
+      />
+    </>
+  );
+
   return (
-    <header className="bg-[#6D071A] text-white shadow-md">
+    <header className="bg-primary text-white shadow-md">
       <div className="container mx-auto px-4 py-3 flex items-center justify-between">
         <div className="flex items-center space-x-8">
-          <Image src={horizontalLogo} alt="NegSud logo"></Image>
-          <nav>
+          <Image src={horizontalLogo} alt="NegoSud logo"></Image>
+          <nav className="hidden lg:flex">
             <ul className="flex space-x-6">
-              <li>
-                <a href="#" className="hover:text-[#D4AF37] transition-colors">
-                  Produits
-                </a>
-              </li>
-              <li>
-                <a href="#" className="hover:text-[#D4AF37] transition-colors">
-                  A propos
-                </a>
-              </li>
-              <li>
-                <a href="#" className="hover:text-[#D4AF37] transition-colors">
-                  Contact
-                </a>
-              </li>
+              <NavItems />
             </ul>
           </nav>
         </div>
         <div className="flex items-center space-x-4">
-          <div className="relative">
-            <Input
-              type="search"
-              placeholder="Rechercher des vins ..."
-              className="pl-10 pr-4 py-2 w-64 rounded-full bg-white text-black"
-            />
-            <SearchIcon
-              className="absolute left-3 top-1/2 transform -translate-y-1/2 text-gray-400"
-              size={20}
-            />
+          <div className="relative hidden lg:flex">
+            <SearchBar />
           </div>
-          <Button variant="ghost" size="icon">
-            <ShoppingCartIcon className="text-white" size={24} />
-          </Button>
-          <Button variant="ghost" size="icon">
-            <UserIcon className="text-white" size={24} />
-          </Button>
+          <Link href="/">
+            <ShoppingCartIcon
+              className="text-white hover:text-secondary"
+              size={24}
+            />
+          </Link>
+          <Link href="/">
+            <UserIcon className="text-white hover:text-secondary" size={24} />
+          </Link>
+          <Dialog>
+            <DialogTrigger className="flex lg:hidden pl-0" asChild>
+              <Button className="hover:text-secondary">
+                <MenuIcon className="text-white" size={24} />
+              </Button>
+            </DialogTrigger>
+            <DialogContent className="bg-primary text-white">
+              <nav className="mb-6">
+                <ul className="space-y-4">
+                  <NavItems />
+                </ul>
+              </nav>
+              <div className="relative">
+                <SearchBar />
+              </div>
+            </DialogContent>
+          </Dialog>
         </div>
       </div>
     </header>
