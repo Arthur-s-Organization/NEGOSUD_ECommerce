661677075e21d98adceab3c272441dea1bf809f7
Nicolas Descarpentries <nicolas.descarp@outlook.fr>
2024-09-22 23:35:12 +0200

    add supplierService + controller

---
+++ API/API/Services/ISupplierService.cs
@@ -0,0 +1,16 @@
+﻿using API.Models.DTOs;
+using API.Models;
+
+namespace API.Services
+{
+	public interface ISupplierService
+	{
+		public Task<Supplier> AddSupplierAsync(SupplierDTO SupplierDTO);
+		public Task<IEnumerable<Supplier>> GetAllSuppliersAsync();
+		public Task<Supplier> GetSupplierByIdAsync(Guid id);
+		public Task<Supplier> UpdateSupplierAsync(Guid id, SupplierDTO SupplierDTO);
+		public Task<Supplier> DeleteSupplierAsync(Guid id);
+		public Task<Supplier> AddAdressToSupplierAsync(Guid SupplierId, Guid AdressId);
+	}
+}
+}
