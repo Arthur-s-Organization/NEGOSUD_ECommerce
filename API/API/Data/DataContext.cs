efc1b2fced4a19f43e606dc6b1b46cc6f4f7d456
Nicolas Descarpentries <nicolas.descarp@outlook.fr>
2024-09-22 22:36:05 +0200

    add DataContext

---
+++ API/API/Data/DataContext.cs
@@ -0,0 +1,24 @@
+﻿using API.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Data
+{
+	public class DataContext : DbContext
+	{
+		public DataContext(DbContextOptions<DataContext> options) : base(options)
+		{
+		}
+
+		public DbSet<Address> Adresses { get; set; }
+		public DbSet<AlcoholFamily> AlcoholFamilies { get; set; }
+		public DbSet<AlcoholItem> AlcoholItems { get; set; }
+		public DbSet<CommonItem> CommonItems { get; set; }
+		public DbSet<Customer> Customers { get; set; }
+		public DbSet<Item> Items { get; set; }
+		public DbSet<Order> Orders { get; set; }
+		public DbSet<OrderDetail> OrderDetails { get; set; }
+		public DbSet<Supplier> Suppliers { get; set; }
+		public DbSet<SupplierOrder> SupplierOrders { get; set; }
+
+	}
+}
