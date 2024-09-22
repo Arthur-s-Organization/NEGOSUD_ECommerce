ed2510ede8acefbfcdabe253b483824caf01ffc3
Nicolas Descarpentries <nicolas.descarp@outlook.fr>
2024-09-22 23:27:11 +0200

    add alcoholItemService + controller

---
+++ API/API/Services/AlcoholItemService.cs
@@ -0,0 +1,100 @@
+﻿using API.Data;
+using API.Models.DTOs;
+using API.Models;
+using AutoMapper;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Services
+{
+	public class AlcoholItemService : IAlcoholItemService
+	{
+		private readonly DataContext _context;
+		private readonly IMapper _mapper;
+
+		public AlcoholItemService(DataContext context, IMapper mapper)
+		{
+			_context = context;
+			_mapper = mapper;
+		}
+		public async Task<AlcoholItem> AddAlcoholItemAsync(AlcoholItemDTO AlcoholItemDTO)
+		{
+			var AlcoholFamily = _context.AlcoholFamilies.SingleOrDefault(af => af.AlcoholFamilyId == AlcoholItemDTO.AlcoholFamilyId);
+
+			if (AlcoholFamily == null)
+			{
+				return null;
+			}
+
+			var Supplier = _context.Suppliers.SingleOrDefault(s => s.SupplierId == AlcoholItemDTO.SupplierId);
+
+			if (Supplier == null)
+			{
+				return null;
+			}
+
+
+			var AlcoholItem = _mapper.Map<AlcoholItem>(AlcoholItemDTO);
+			await _context.AlcoholItems.AddAsync(AlcoholItem);
+			await _context.SaveChangesAsync();
+
+			return AlcoholItem;
+		}
+
+		public async Task<AlcoholItem> DeleteAlcoholItemAsync(Guid id)
+		{
+			var AlcoholItem = await _context.AlcoholItems.SingleOrDefaultAsync(ai => ai.ItemId == id);
+			if (AlcoholItem is null)
+			{
+				return null;
+			}
+			_context.AlcoholItems.Remove(AlcoholItem);
+			await _context.SaveChangesAsync();
+			return AlcoholItem;
+		}
+
+		public async Task<AlcoholItem> GetAlcoholItemByIdAsync(Guid id)
+		{
+			var AlcoholItem = await _context.AlcoholItems
+				.Include(ai => ai.AlcoholFamily)
+				.Include(ai => ai.Supplier)
+				.SingleOrDefaultAsync(ai => ai.ItemId == id);
+			if (AlcoholItem is null)
+			{
+				return null;
+			}
+			return AlcoholItem;
+		}
+
+		public async Task<IEnumerable<AlcoholItem>> GetAllAlcoholItemsAsync()
+		{
+			var AlcoholItems = await _context.AlcoholItems
+				.Include(ai => ai.AlcoholFamily)
+				.Include(ai => ai.Supplier)
+				.ToListAsync();
+			return AlcoholItems;
+		}
+
+		public async Task<AlcoholItem> UpdateAlcoholItemAsync(Guid id, AlcoholItemDTO AlcoholItemDTO)
+		{
+			var existingAlcoholItem = await _context.AlcoholItems.FindAsync(id);
+
+			if (existingAlcoholItem == null)
+			{
+				return null;
+			}
+
+			var existingAlcoholFamily = await _context.AlcoholFamilies.FindAsync(AlcoholItemDTO.AlcoholFamilyId);
+
+			if (existingAlcoholFamily == null)
+			{
+				return null;
+			}
+
+			_mapper.Map(AlcoholItemDTO, existingAlcoholItem);
+
+			await _context.SaveChangesAsync();
+			return existingAlcoholItem;
+		}
+
+	}
+}
