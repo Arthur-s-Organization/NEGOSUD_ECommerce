a55c56f5e65d0067d23e5043baeeb01e2f45e33b
JeanneGrenet <jeanne.grenet@gmail.com>
2024-09-27 15:48:40 +0200

    fix : clean front

--- APP/negosud_front/src/app/page.tsx
+++ APP/negosud_front/src/app/page.tsx
@@ -1,12 +1,10 @@
-"use client"
+"use client";
 import Hero from "@/components/Hero";
-import Test from "@/components/test";
 
 export default function Home() {
   return (
     <>
       <Hero />
-      <Test/>
     </>
   );
 }
