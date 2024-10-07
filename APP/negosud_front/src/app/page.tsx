ecad0c88973b4346d74fc0e0eba7c314f09386b6
JeanneGrenet <jeanne.grenet@gmail.com>
2024-10-07 09:32:34 +0200

    feat : create new item

--- APP/negosud_front/src/app/page.tsx
+++ APP/negosud_front/src/app/page.tsx
@@ -16,15 +16,26 @@ export default function Home() {
   return (
     <>
       <Hero />
-      <div className="py-12 max-w-6xl mx-auto flex flex-col gap-4">
-        <h2 className="font-heading font-bold text-2xl">
-          Les meilleures ventes :
-        </h2>
-        <div className="flex gap-x-2 items-center">
-          <ProductCard product={product} />
-          <ProductCard product={product} />
-          <ProductCard product={product} />
-          <ProductCard product={product} />
+      <div className="py-12 max-w-6xl mx-auto flex flex-col gap-12">
+        <div className="flex flex-col gap-4">
+          <h2 className="font-heading font-bold text-2xl">
+            Les meilleures ventes :
+          </h2>
+          <div className="flex gap-x-2 items-center">
+            <ProductCard product={product} />
+            <ProductCard product={product} />
+            <ProductCard product={product} />
+            <ProductCard product={product} />
+          </div>
+        </div>
+        <div className="flex flex-col gap-4">
+          <h2 className="font-heading font-bold text-2xl">Nouveautés :</h2>
+          <div className="flex gap-x-2 items-center">
+            <ProductCard product={product} />
+            <ProductCard product={product} />
+            <ProductCard product={product} />
+            <ProductCard product={product} />
+          </div>
         </div>
       </div>
     </>
