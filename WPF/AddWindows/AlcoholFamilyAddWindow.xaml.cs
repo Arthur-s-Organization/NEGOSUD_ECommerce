adb586d2bbfe439958cbeb78f17008cb516ed3bc
JeanneGrenet <jeanne.grenet@gmail.com>
2024-10-10 14:45:31 +0200

    ARTHUR : Added Create and Update functionnalities on WPF

---
+++ WPF/AddWindows/AlcoholFamilyAddWindow.xaml.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Net.Http;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Xml.Linq;
+using WPF.Class;
+
+namespace WPF
+{
+    public partial class AlcoholFamilyAddWindow : Window
+    {
+        public AlcoholFamilyRequestDTO AlcohoFamily { get; private set; }
+
+        public AlcoholFamilyAddWindow()
+        {
+            InitializeComponent();
+        }
+
+        private async void SaveButton_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                // Valider et sauvegarder les modifications dans un objet AlcoholFamilyRequestDTO
+                AlcohoFamily = new AlcoholFamilyRequestDTO
+                {
+                    Name = Name.Text
+                };
+
+                // Convertir l'objet en JSON
+                string jsonItem = JsonSerializer.Serialize(AlcohoFamily);
+                var content = new StringContent(jsonItem, Encoding.UTF8, "application/json");
+
+                // Envoyer la requête PUT à l'API
+                using (HttpClient client = new HttpClient())
+                {
+                    var response = await client.PostAsync($"https://localhost:7246/api/AlcoholFamily", content);
+
+                    // Vérifier si la requête a réussi
+                    if (response.IsSuccessStatusCode)
+                    {
+                        MessageBox.Show("Alcohol Family updated successfully!", "Success", MessageBoxButton.OK, MessageBoxImage.Information);
+                        DialogResult = true;
+                    }
+                    else
+                    {
+                        MessageBox.Show($"Error: {response.StatusCode}", "Update Failed", MessageBoxButton.OK, MessageBoxImage.Error);
+                        DialogResult = false;
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"An error occurred: {ex.Message}", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+
+            // Fermer la fenêtre
+            Close();
+        }
+    }
+}
---
+++ WPF/EditWindows/SupplierEditWindow.xaml.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Net.Http;
+using System.Net.Http.Json;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Xml.Linq;
+using WPF.Class;
+
+namespace WPF
+{
+    public partial class SupplierEditWindow: Window
+    {
+        private Guid supplierId;
+        private static readonly HttpClient client = new HttpClient();
+        private List<AddressResponseDTO> addresses;
+        public SupplierRequestDTO Supplier { get; set; }
+
+        public SupplierEditWindow(SupplierResponseDTO supplier)
+        {
+            InitializeComponent();
+            LoadDataAsync();
+
+            if (supplier != null)
+            {
+                Name.Text = supplier.Name;
+                Description.Text = supplier.Description;
+                PhoneNumber.Text = supplier.PhoneNumber;
+                AddressComboBox.SelectedValue = supplier.Address.AddressId;
+                supplierId = supplier.SupplierId;
+            }
+        }
+
+        private async Task LoadDataAsync()
+        {
+            try
+            {
+                // Charger les adresses
+                addresses = await client.GetFromJsonAsync<List<AddressResponseDTO>>("https://localhost:7246/api/Adress");
+
+                // Lier les données aux ComboBox
+                AddressComboBox.ItemsSource = addresses;
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show($"Erreur lors du chargement des données : {e.Message}");
+            }
+        }
+
+        private async void SaveButton_Click(object sender, RoutedEventArgs e)
+        {
+            try { 
+                Supplier = new SupplierRequestDTO
+                {
+                    Name = Name.Text,
+                    Description = Description.Text,
+                    PhoneNumber = PhoneNumber.Text,
+                    AddressId = (Guid) AddressComboBox.SelectedValue
+                };
+                // Convertir l'objet en JSON
+                string jsonItem = JsonSerializer.Serialize(Supplier);
+                var content = new StringContent(jsonItem, Encoding.UTF8, "application/json");
+
+                // Envoyer la requête PUT à l'API
+                using (HttpClient client = new HttpClient())
+                {
+                    var response = await client.PutAsync($"https://localhost:7246/api/Supplier/{supplierId}", content);
+
+                    // Vérifier si la requête a réussi
+                    if (response.IsSuccessStatusCode)
+                    {
+                        MessageBox.Show("Supplier updated successfully!", "Success", MessageBoxButton.OK, MessageBoxImage.Information);
+                        DialogResult = true;
+                    }
+                    else
+                    {
+                        MessageBox.Show($"Error: {response.StatusCode}", "Update Failed", MessageBoxButton.OK, MessageBoxImage.Error);
+                        DialogResult = false;
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"An error occurred: {ex.Message}", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+
+            // Fermer la fenêtre
+            Close();
+        }
+    }
+}
